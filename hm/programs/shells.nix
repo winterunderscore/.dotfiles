985b9374c0aa71afb8d5f12d8303e8ffa3720687
winterunderscore <66862891+winterunderscore@users.noreply.com>
2023-10-18 12:35:10 +0000

    starship stuff

--- hm/programs/shells.nix
+++ hm/programs/shells.nix
@@ -41,6 +41,18 @@ in {
     starship = {
       enable = true;
       enableBashIntegration = true;
+      enableFishIntegration = true;
+      settings = {
+        command_timeout = 5000;
+        character = {
+          success_symbol = "[λ](bold green)";
+          error_symbol = "[λ](bold red)";
+        };
+        directory = {
+          fish_style_pwd_dir_length = 2;
+          truncate_to_repo = false;
+        };
+      };
     };
   };
 }
